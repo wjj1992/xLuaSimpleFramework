07eb44cc594bd6824de04206e253fdc993ec6a76
rong.ye <yerongsc@163.com>
2017-02-15 23:34:25 +0800

    Init

---
+++ Assets/XLua/Examples/06_Coroutine/Coroutine_Runner.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using XLua;
+using System.Collections.Generic;
+using System.Collections;
+using System;
+
+[LuaCallCSharp]
+public class Coroutine_Runner : MonoBehaviour
+{
+    public void YieldAndCallback(object to_yield, Action callback)
+    {
+		Debug.Log("yield call back");
+        StartCoroutine(CoBody(to_yield, callback));
+    }
+
+    private IEnumerator CoBody(object to_yield, Action callback)
+    {
+        if (to_yield is IEnumerator)
+            yield return StartCoroutine((IEnumerator)to_yield);
+        else
+            yield return to_yield;
+		Debug.Log("co body callback");
+        callback();
+    }
+}
+
+public static class CoroutineConfig
+{
+    [LuaCallCSharp]
+    public static List<Type> LuaCallCSharp
+    {
+        get
+        {
+            return new List<Type>()
+            {
+                typeof(WaitForSeconds),
+                typeof(WWW)
+            };
+        }
+    }
+}
---
+++ Assets/XLua/Test/HotFixMethod/HotFixMemberMethodTest.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using XLua;
+using System.Collections.Generic;
+[Hotfix]
+public class HotFixMemberMethodTest : MonoBehaviour {
+
+	public int a = 10;
+	public List<int> intList = new List<int>();
+	public GameObject go;
+	void Awake()
+	{
+		go = new GameObject("xxx");	
+		LuaEnv luaenv = new LuaEnv();
+		luaenv.DoString(@"
+			local util = require 'xlua.util'
+    		xlua.hotfix(CS.HotFixMemberMethodTest,{
+     	   Log = function(self)
+            print('----AfterFix--------')          
+ 		    print('Log In Lua')
+			self.a = 100
+			local intList = self.intList
+			intList:Add(123)
+			--print(self.strList)
+			--self.strList.Add(2)
+			print(self.a)
+        end;
+  		Co_Test = function(self)
+            return util.cs_generator(
+			function()
+                --while true do
+					print('lua coroutine')
+                    coroutine.yield(CS.UnityEngine.WaitForSeconds(3))
+                    print('Wait for 3 seconds')
+                --end             
+            end)
+        end;
+			
+    })");
+	}
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Start in C#");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(intList.Count);
+	}
+
+	public void Log()
+	{	
+		Debug.Log("-----BeforeFix------");	
+		Debug.Log("Log In C#");
+	}
+
+	IEnumerator Co_Test()
+	{
+		yield return new WaitForSeconds(3.0f);
+		Debug.Log("Log in C# Cortotine");
+	}
+
+	void OnGUI()
+	{
+		if(GUI.Button(new Rect(Screen.width/2,200,100,100),"Log"))
+		{
+			Log();
+			StartCoroutine(Co_Test());
+		}	
+	}
+}
